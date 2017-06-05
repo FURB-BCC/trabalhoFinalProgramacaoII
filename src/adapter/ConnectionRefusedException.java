93b7db4ebe73e4054a6b46f1e887cfd141fea00b
renatoconstanciofilho <renatoconstanciofilho@gmail.com>
2017-06-05 17:06:12 -0300

    Façade parcialmente implementado

---
+++ src/adapter/ConnectionRefusedException.java
@@ -0,0 +1,33 @@
+package adapter;
+
+public class ConnectionRefusedException extends Exception {
+
+	public ConnectionRefusedException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConnectionRefusedException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConnectionRefusedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConnectionRefusedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConnectionRefusedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
--- src/adapter/PesquisaPrecosFacade.java
+++ src/adapter/PesquisaPrecosFacade.java
@@ -1,5 +1,53 @@
 package adapter;
 
+import java.util.ArrayList;
+
+import model.CD;
+
 public class PesquisaPrecosFacade {
+	
+	private ArrayList<CD> resultados = new ArrayList<CD>();		
+	private SomLivreAdapter somLivreAdapter;
+	private SubmarinoAdapter submarinoAdapter;
+	
+	public ArrayList pesquisar(String chave) throws Exception{
+//		Connectar, carregar todos os CDs da loja SomLivre e descontectar
+		if(somLivreAdapter.conectar("furb", "furb"))
+			resultados.addAll(somLivreAdapter.procurar("todos"));
+		else
+			throw new ConnectionRefusedException();
+		somLivreAdapter.desconectar();
+		
+//		Connectar, carretar todos os CDs da loja Submarino e desconectar
+		if(submarinoAdapter.conectar("furb", "furb"))
+			resultados.addAll(submarinoAdapter.procurar("todos"));
+		else
+			throw new ConnectionRefusedException();
+		submarinoAdapter.desconectar();
+		
+//		Criado ArrayList de retorno
+		ArrayList<CD> resultadoPesquisa = new ArrayList<>();
+		
+//		Pesquisar o termo no artista e/ou título do cd
+		for (CD cd : resultados) {
+			if(cd.getArtista().matches(chave)){
+				resultadoPesquisa.add(cd);
+			}
+			if(!resultadoPesquisa.contains(cd) && cd.getTitulo().matches(chave)){
+				resultadoPesquisa.add(cd);
+			}
+			else
+				return resultadoPesquisa;
+		}
+		return resultadoPesquisa;
+	}
+	
+	public void salvar(){
+		
+	}
+	
+	public void ler(){
+		
+	}
 
 }

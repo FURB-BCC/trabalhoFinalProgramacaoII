055356ecc4e35daf19fcdb7ddcad99c56b230885
Pedro <pedrohalba@hotmail.com>
2017-06-06 10:42:03 -0300

    Serialização

--- src/facade/PesquisaPrecosFacade.java
+++ src/facade/PesquisaPrecosFacade.java
@@ -1,6 +1,14 @@
 package facade;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import adapter.SomLivreAdapter;
 import adapter.SubmarinoAdapter;
@@ -40,12 +48,28 @@ public class PesquisaPrecosFacade {
 		return resultados;
 	}
 	
-	public void salvar(){
+	public void salvar(String key, Collection cdsPesquisados, String data) throws FileNotFoundException, IOException{
 		
+		File file = new File("C:\\Temp\\TrabalhoFinalProgramacaoII.log");
+		
+		ObjectOutputStream output = new ObjectOutputStream(
+				new FileOutputStream(file));
+
+		FileWriter buff = new FileWriter(file,true);
+		
+		buff.write(key + " " + data);
+		buff.close();
+	
+		output.writeObject(cdsPesquisados);		
+		output.close();
+		
+		buff = new FileWriter(file,true);
+		buff.write(" \\" + key);
+		buff.close();
 	}
 	
-	public void ler(){
-		
+	public Collection ler(){
+		return null;
 	}
 
 }
--- src/view/Tela.java
+++ src/view/Tela.java
@@ -2,10 +2,17 @@ package view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -23,6 +30,7 @@ public class Tela extends JFrame {
 	private javax.swing.JTextField txtInput;
 	private DefaultTableModel modeloTabela;
 	private PesquisaPrecosFacade pesquisaFacade;
+	private ArrayList<CD> cdsPesquisados;
 	// End of variables declaration
 
 	public static void main(String[] args) throws Exception {
@@ -37,6 +45,8 @@ public class Tela extends JFrame {
 
 	private void initComponents() {
 
+		//cdsPesquisados = (ArrayList<CD>) pesquisaFacade.ler();
+		cdsPesquisados = new ArrayList<>();
 		btnPesquisar = new javax.swing.JButton();
 		btnPesquisar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -62,28 +72,59 @@ public class Tela extends JFrame {
 						new String[] { "Titulo", "Banda/Artista", "Preço", "Loja" }));
 
 		jScrollPane.setViewportView(jTable);
+		
+		JButton btSalvar = new JButton("Salvar");
+		btSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try{
+					salvar();
+				}catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Erro! " + e.getMessage());
+				}
+			}
+		});
 
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-		layout.setHorizontalGroup(layout.createParallelGroup(Alignment.LEADING).addGroup(layout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(layout.createParallelGroup(Alignment.LEADING)
-						.addComponent(jScrollPane, GroupLayout.DEFAULT_SIZE, 634, Short.MAX_VALUE).addGroup(
-								Alignment.TRAILING,
-								layout.createSequentialGroup()
-										.addComponent(txtInput, GroupLayout.PREFERRED_SIZE, 99,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.RELATED, 456, Short.MAX_VALUE)
-										.addComponent(btnPesquisar)))
-				.addContainerGap()));
-		layout.setVerticalGroup(layout.createParallelGroup(Alignment.LEADING).addGroup(layout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(btnPesquisar).addComponent(
-						txtInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(ComponentPlacement.UNRELATED)
-				.addComponent(jScrollPane, GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE).addContainerGap()));
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(layout.createParallelGroup(Alignment.LEADING)
+						.addComponent(jScrollPane, GroupLayout.DEFAULT_SIZE, 634, Short.MAX_VALUE)
+						.addGroup(layout.createSequentialGroup()
+							.addComponent(txtInput, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 371, Short.MAX_VALUE)
+							.addComponent(btnPesquisar)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btSalvar)))
+					.addContainerGap())
+		);
+		layout.setVerticalGroup(
+			layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txtInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnPesquisar)
+						.addComponent(btSalvar))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(jScrollPane, GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE)
+					.addContainerGap())
+		);
 		getContentPane().setLayout(layout);
 
 		pack();
+	}
+
+	protected void salvar() throws IOException {
+		String key = JOptionPane.showInputDialog("Informe a chave de salvamento");
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		String data = dtf.format(now); //2016/11/16 12:08:43
+		
+		pesquisaFacade.salvar(key, cdsPesquisados, data);
+		
 	}
 
 	protected void pesquisar() throws Exception {
@@ -93,9 +134,11 @@ public class Tela extends JFrame {
 		
 		for (Object object : retorno) {
 			CD cd = (CD) object;
+			if(cdsPesquisados != null)
+				cdsPesquisados.add(cd);
 			modeloTabela.addRow(new String[] {cd.getTitulo(), cd.getArtista(),String.valueOf(cd.getPreco()), cd.getLoja()});
 		}
 		
 	}
-
 }
+	
